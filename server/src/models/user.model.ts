33763e80b8cd84bbacf1b8069860df3fb4181b3a
Kavindraramdeni <kavindraraj1703@gmail.com>
2025-11-09 16:51:48 +0530

    Update user.model.ts

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -13,16 +13,16 @@ const userSchema: Schema<IUser> = new Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
 
-// Method to compare passwords
-userSchema.methods.matchPassword = async function (enteredPassword: string) {
-    return await bcrypt.compare(enteredPassword, this.password);
+// Compare password method
+userSchema.methods.matchPassword = async function(password: string) {
+    return await bcrypt.compare(password, this.password);
 };
 
 const User = mongoose.model<IUser>('User', userSchema);
