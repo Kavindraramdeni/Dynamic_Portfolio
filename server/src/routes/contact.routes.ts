42242b184959acd1a99074a04db52d47869e5734
Kavindraramdeni <kavindraraj1703@gmail.com>
2025-11-09 11:59:37 +0530

    Delete server/src directory

--- server/src/routes/contact.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router,RequestHandler } from 'express';
-import { sendContactMessage } from '../controllers/contact.controller.ts';
-
-const router = Router();
-
-router.post('/contact', sendContactMessage as RequestHandler);
-
-export default router;
